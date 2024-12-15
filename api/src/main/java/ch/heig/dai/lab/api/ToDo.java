8e7a48a987349617601e63c176e79b2da717904b
SDY <dylan.stegmuller@heig-vd.ch>
2024-12-15 15:36:02 +0100

    Step 3: HTTP API serve, initialized project

---
+++ api/src/main/java/ch/heig/dai/lab/api/ToDo.java
@@ -0,0 +1,13 @@
+package ch.heig.dai.lab.api;
+
+/**
+ * Hello world!
+ *
+ */
+public class ToDo 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
